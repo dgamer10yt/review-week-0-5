b2b811a0f5a2e871bdb449eaa115422c2964ccdb
dgamer10 <79217683+dgamer10yt@users.noreply.github.com>
2022-12-17 11:25:34 +0200

    Add files via upload

---
+++ parcAuto.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct Masina
+{
+    char* marca;
+    char *model;
+    char tokenMasina[11];
+    int pretAchizitie;
+    int pretVanzare
+}Masina;
+
+int main(int main argc, char *argv[])
+{
+    FILE *inputCerinte = fopen(argv[1], "r")
+}
